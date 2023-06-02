980a0fbd4a39660183a6edd64221f90694536f8b
SidonTheTroll <70560640+SidonTheTroll@users.noreply.github.com>
2023-06-02 17:51:13 +0530

    Added IP notes

--- python_prototypes.py
+++ python_prototypes.py
@@ -2,17 +2,9 @@
 #  This is a prototype program file to test code for efficiency of code
 
 
-# Get user input
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-num3 = int(input("Enter the third number: "))
+x = int(input("What is the base? \n"))
+y = int(input("What is the power \n")) 
 
-def descending_order(a, b, c):
-    numbers = [a, b, c]
-    numbers.sort(reverse=True)
-    return numbers
+a = x**y 
 
-
-# Call the function and print the result
-result = descending_order(num1, num2, num3)
-print("Numbers in descending order:", result)
+print(f"The answer is {a}")
