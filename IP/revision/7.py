e2d53ec16c0a00d18e048584ebf574f8bf8e6ead
SidonTheTroll <70560640+SidonTheTroll@users.noreply.github.com>
2023-06-18 10:38:15 +0530

    added a single ip question

---
+++ IP/revision/7.py
@@ -0,0 +1,9 @@
+# def convert(): 
+#     return dist/time 
+
+dist = float(input("Enter distance covered in kilometers: "))
+time = float(input("Enter time taken in hours: ")) 
+
+# print(f"The speed is {convert()} kmph.")
+
+print(f"The speed is {dist/time} kmph.")
